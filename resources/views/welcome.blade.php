28fbb5a55b4aebde776515be179c6694b23b5030
Alberto Rodellar <arodellar99@gmail.com>
2026-02-19 23:39:25 +0100

    feat: Pass data to views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,9 @@
+{{-- Al welcome le hemos pasado dos variables $greeting y $person que viene de la url
+Tambien se puede usar {{!!  !!}} para mostrar html sin escapar, pero hay que tener cuidado con esto para evitar ataques XSS
+Ej: poner ?person=<script>alert('XSS')</script> en la url, si usamos {{!! $person !!}}
+    se ejecutaría el script, pero con {{ $person }} se mostraría como texto sin ejecutar el script
+--}}
 <x-layout title="Welcome">
     <h1>Hello World!</h1>
+    <p>{{ $greeting }}, {{ $person }}!</p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 // En vez de route get return view se puede hacer con una sola linea usando Route::view
 // Route::view('/url', 'view-name');
-Route::view('/', 'welcome');
+
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
+
+// Pasar datos a una vista con datos en la url ?person=Alberto
+Route::get('/', function () {
+    return view('welcome', [
+        'greeting' => 'Hello',
+        'person' => request('person', 'World')
+    ]);
+});
